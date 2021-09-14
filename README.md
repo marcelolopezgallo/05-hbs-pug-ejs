e6c4d200125d3999be009b5628a36a9c94e80bd9
Marcelo Lopez Gallo <melopez@teco.com.ar>
2021-09-14 09:52:33 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,1 @@
+# 05-hbs-pug-ejs
---
+++ ejs/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+const indexRouter = require('./routes/index')
+const productsRouter = require('./routes/products')
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+//app.use(express.static('public'))
+
+app.use('/', indexRouter)
+app.use('/products', productsRouter)
+
+const PORT = process.env.PORT || 8080
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+})
+server.on('error', error => console.log(`Error en servidor ${error}`))
